7b411009f3fae1e57b83922415e2daca0cf97407
Wu JunYi <wu65830600@163.com>
2022-05-14 22:35:26 +0800

    add syn attack

---
+++ synAttack/server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	if err := r.Run("0.0.0.0:18081"); err != nil {
+		fmt.Println(err)
+	}
+}
